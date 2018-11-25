5c47501a82cee2d75b567fb67dafb1c00620c7dc
glynnkuo <glynn.kuo@gmail.com>
2018-11-25 20:41:25 +0800

    refactor

--- rs_pchm_query.py
+++ rs_pchm_query.py
@@ -9,6 +9,85 @@ import pyodbc
 import datetime
 import base64
 
+datetime_format='%Y-%m-%dT%H:%M:%S'
+
+def people_count(form, db_cursor):
+    q_cid = form.getvalue('cid')
+    q_from = datetime.datetime.strptime(form.getvalue('from'), datetime_format)
+    q_to = datetime.datetime.strptime(form.getvalue('to'), datetime_format)
+
+    sql_people_count = """
+        SELECT m.DeviceId, sum(cast(m.Data as int))
+          FROM VAS_Retail_Package.dbo.VAS_IVS_MetaData AS m
+         WHERE m.Type = '%s'
+           AND m.LocalTime BETWEEN '%s' AND '%s'
+           AND m.DeviceId = '%s'
+      GROUP BY m.DeviceId
+    """
+    
+    out_data = {}
+    out_data['in'] = 0
+    out_data['out'] = 0
+    #people in
+    db_cursor.execute(sql_people_count%(
+      'People In',
+      q_from.strftime(datetime_format),
+      q_to.strftime(datetime_format),
+      q_cid))
+    row = db_cursor.fetchone()
+    if row:
+        out_data['in']  = row[1]
+
+    #people out
+    db_cursor.execute(sql_people_count%(
+      'People Out',
+      q_from.strftime(datetime_format),
+      q_to.strftime(datetime_format),
+      q_cid))
+    row = db_cursor.fetchone()
+    if row:
+        out_data['out'] = row[1]
+    return 'application/json', json.dumps(out_data)
+
+def heatmap(form, db_cursor):
+    q_cid = form.getvalue('cid')
+    q_from = datetime.datetime.strptime(form.getvalue('from'), datetime_format)
+    q_to = datetime.datetime.strptime(form.getvalue('to'), datetime_format)
+
+    sql_heatmap = """
+      SELECT m.RecordId, m.LocalTime
+        FROM VAS_Retail_Package.dbo.VAS_IVS_MetaData AS m
+       WHERE m.Type = 'HeatMapMetaData'
+         AND m.LocalTime BETWEEN '%s' AND '%s'
+         AND m.DeviceId = '%s'
+    """
+    db_cursor.execute(sql_heatmap%(
+      q_from.strftime(datetime_format),
+      q_to.strftime(datetime_format),
+      q_cid))
+      
+    out_data = {}
+    out_data['heatmap'] = []
+    while True:
+        row = db_cursor.fetchone()
+        if not row:
+            break
+        out_data['heatmap'].append({'time': row[1].strftime(datetime_format), 'hmid': row[0]})
+        
+    return 'application/json', json.dumps(out_data)
+    
+def heatmap_data(form, db_cursor):
+    sql_heatmap_data = """
+      SELECT b.Data
+        FROM VAS_Retail_Package.dbo.MAS_IVS_BinaryData AS b
+       WHERE b.MetaDataId = %d
+    """
+    db_cursor.execute(sql_heatmap%q_hmid)
+    row = db_cursor.fetchone()
+    if row:
+        out_data['data': row[0]]
+
+    
 def main():
     db_ip     = '192.168.225.253'
     db_name   = 'VAS_Retail_Package'
@@ -20,81 +99,18 @@ def main():
 
     form = cgi.FieldStorage()
     q_type = form.getvalue('type')
-    q_cid = form.getvalue('cid')
-    datetime_format='%Y-%m-%dT%H:%M:%S'
-    q_from = datetime.datetime.strptime(form.getvalue('from'), datetime_format)
-    q_to = datetime.datetime.strptime(form.getvalue('to'), datetime_format)
 
-    out_data = {'type': q_type, 'cid': q_cid, 'from': q_from.strftime(datetime_format), 'to': q_to.strftime(datetime_format)}
-
-    sql_people_count = """
-        SELECT m.DeviceId, sum(cast(m.Data as int))
-          FROM VAS_Retail_Package.dbo.VAS_IVS_MetaData AS m
-         WHERE m.Type = '%s'
-           AND m.LocalTime BETWEEN '%s' AND '%s'
-           AND m.DeviceId = '%s'
-      GROUP BY m.DeviceId"""
-      
-    sql_heatmap = """
-        SELECT m.RecordId, m.LocalTime
-          FROM VAS_Retail_Package.dbo.VAS_IVS_MetaData AS m
-         WHERE m.Type = 'HeatMapMetaData'
-           AND m.LocalTime BETWEEN '%s' AND '%s'
-           AND m.DeviceId = '%s'
-    """
-    sql_heatmap_data = """
-        SELECT b.Data
-          FROM VAS_Retail_Package.dbo.MAS_IVS_BinaryData AS b
-         WHERE b.MetaDataId = %d
-    """
-
-
+    content_type = ""
+    body = None
     if q_type == 'people_count':
-        out_data['in'] = 0
-        out_data['out'] = 0
-        #people in
-        db_cursor.execute(sql_people_count%(
-          'People In',
-          q_from.strftime(datetime_format),
-          q_to.strftime(datetime_format),
-          q_cid))
-        row = db_cursor.fetchone()
-        if row:
-            out_data['cid'] = row[0]
-            out_data['in']  = row[1]
-
-        #people out
-        db_cursor.execute(sql_people_count%(
-          'People Out',
-          q_from.strftime(datetime_format),
-          q_to.strftime(datetime_format),
-          q_cid))
-        row = db_cursor.fetchone()
-        if row:
-            out_data['cid'] = row[0]
-            out_data['out'] = row[1]
-
+        content_type, body = people_count(form, db_cursor)
     elif q_type == 'heatmap':
-        db_cursor.execute(sql_heatmap%(
-          q_from.strftime(datetime_format),
-          q_to.strftime(datetime_format),
-          q_cid))
-        out_data['heatmap'] = []
-        while True:
-            row = db_cursor.fetchone()
-            if not row:
-                break
-            out_data['heatmap'].append({'time': row[1].strftime(datetime_format), 'hmid': row[0]})
+        content_type, body = heatmap(form, db_cursor)
     elif q_type == 'heatmap_data':
-        db_cursor.execute(sql_heatmap%q_hmid)
-        row = db_cursor.fetchone()
-        if row:
-            out_data['data': row[0]]
-
-    body = json.dumps(out_data)
+        content_type, body = heatmap_data(form, db_cursor)
 
     print "Status: 200 OK"
-    print "Content-Type: application/json"
+    print "Content-Type: %s"%content_type
     print "Length:", len(body)
     print ""
     print body
